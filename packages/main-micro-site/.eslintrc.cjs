3109cf670c1b36270e085d09b6915d44d96ec36c
xiaoyaochen <xiaoyaochen@crland.com.cn>
2024-03-07 20:07:11 +0800

    修改eslint包名与npm库对应

--- packages/main-micro-site/.eslintrc.cjs
+++ packages/main-micro-site/.eslintrc.cjs
@@ -1,10 +1,10 @@
 module.exports={
-  extends: ['@httpxyc/eslint-config'],
+  extends: ['@mozartchen/eslint-config'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   overrides: [
     {
       files: ['*.ts','*.tsx'],
-      extends: ['@httpxyc/eslint-config/react','@httpxyc/eslint-config/typescript'],
+      extends: ['@mozartchen/eslint-config/react','@mozartchen/eslint-config/typescript'],
       // 有这个，才能正确解析ts类型文件，告诉解析器ts配置文件位置，不然ts/tsx文件的开头会报parsing error: Cannot read file '...../tsconfig.json'错误
       parserOptions: {
         tsconfigRootDir: __dirname,
